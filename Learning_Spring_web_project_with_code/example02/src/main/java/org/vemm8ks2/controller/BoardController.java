fc4da2173a5d3475f81c7ad526f51cc9c859ecd0
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-22 10:38:25 +0900

    FEAT: Chapter 10. 프레젠테이션(웹) 계층의 CRUD 구현 (10.2)
    10.2 BoardController 작성

---
+++ Learning_Spring_web_project_with_code/example02/src/main/java/org/vemm8ks2/controller/BoardController.java
@@ -0,0 +1,13 @@
+package org.vemm8ks2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+public class BoardController {
+
+}
