aa7d04b3d0b5ef3a57e596b77df41dc2245ad04f
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-22 10:59:40 +0900

    FEAT, TEST: Chapter 10. 프레젠테이션(웹) 계층의 CRUD 구현 (10.2.1)
    10.2 BoardController 작성
    10.2.1 목록에 대한 처리와 테스트

--- Learning_Spring_web_project_with_code/example02/src/main/java/org/vemm8ks2/controller/BoardController.java
+++ Learning_Spring_web_project_with_code/example02/src/main/java/org/vemm8ks2/controller/BoardController.java
@@ -1,13 +1,27 @@
 package org.vemm8ks2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.vemm8ks2.service.BoardService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/board/*")
+@AllArgsConstructor
 public class BoardController {
 
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		
+		model.addAttribute("list", service.getList());
+	}
 }
