bfa9eb712b26841bc0a19abdf0b0a2496e4c50a2
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-23 17:19:34 +0900

    TEST: Chapter 13. MyBatis와 스프링에서 페이징 처리 (13.1.1)
    13.1 MyBatis 처리와 테스트
    13.1.1 페이징 테스트와 수정

--- Learning_Spring_web_project_with_code/example02/src/test/java/org/vemm8ks2/mapper/BoardMapperTests.java
+++ Learning_Spring_web_project_with_code/example02/src/test/java/org/vemm8ks2/mapper/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.vemm8ks2.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.vemm8ks2.domain.BoardVO;
+import org.vemm8ks2.domain.Criteria;
 
 import lombok.extern.log4j.Log4j;
 
@@ -81,6 +84,19 @@ public class BoardMapperTests {
 		
 		log.info("UPDATE COUNT: " + cnt);
 		
+	}
+	
+	@Test
+	public void testPaging() {
+		
+		Criteria cri = new Criteria();
+		
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(list));
 	}
 	
 }
