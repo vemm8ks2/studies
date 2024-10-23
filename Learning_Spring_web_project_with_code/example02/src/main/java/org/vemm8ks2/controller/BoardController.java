5ad33a44dfd06dd60854778d17a1f23a9fedb512
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-23 17:35:05 +0900

    FEAT, TEST: Chapter 13. MyBatis와 스프링에서 페이징 처리 (13.2.2)
    13.2 BoardController와 BoardService 수정
    13.2.2 BoardController 수정

--- Learning_Spring_web_project_with_code/example02/src/main/java/org/vemm8ks2/controller/BoardController.java
+++ Learning_Spring_web_project_with_code/example02/src/main/java/org/vemm8ks2/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.vemm8ks2.domain.BoardVO;
+import org.vemm8ks2.domain.Criteria;
 import org.vemm8ks2.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -22,11 +23,11 @@ public class BoardController {
 	private BoardService service;
 	
 	@GetMapping("/list")
-	public void list(Model model) {
+	public void list(Criteria cri, Model model) {
 		
 		log.info("list");
 		
-		model.addAttribute("list", service.getList());
+		model.addAttribute("list", service.getList(cri));
 	}
 	
 	@GetMapping("/register")
