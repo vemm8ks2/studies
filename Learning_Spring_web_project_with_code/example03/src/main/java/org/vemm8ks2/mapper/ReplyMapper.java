749ec9777d5c0ccae960aa046428471ca35cf703
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 17:40:46 +0900

    FEAT, TEST: 17. Ajax 댓글 처리 (17.2.3)
    17.2 댓글 처리를 위한 영속 영역
    17.2.3 CRUD 작업
    
    등록(create)

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/mapper/ReplyMapper.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/mapper/ReplyMapper.java
@@ -1,5 +1,8 @@
 package org.vemm8ks2.mapper;
 
+import org.vemm8ks2.domain.ReplyVO;
+
 public interface ReplyMapper {
 
+  public int insert(ReplyVO vo);
 }
--- Learning_Spring_web_project_with_code/example03/src/test/java/org/vemm8ks2/mapper/ReplyMapperTests.java
+++ Learning_Spring_web_project_with_code/example03/src/test/java/org/vemm8ks2/mapper/ReplyMapperTests.java
@@ -1,10 +1,12 @@
 package org.vemm8ks2.mapper;
 
+import java.util.stream.IntStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.vemm8ks2.domain.ReplyVO;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringRunner.class)
@@ -14,10 +16,27 @@ public class ReplyMapperTests {
 
   @Autowired
   private ReplyMapper mapper;
+  
+  private Long[] bnoArr = { 524312L, 524311L, 524310L, 524309L, 524308L }; 
 
   @Test
   public void testMapper() {
     
     log.info(mapper);
+  }
+  
+  @Test
+  public void testCreate() {
+    
+    IntStream.rangeClosed(1, 10).forEach(i -> {
+      
+      ReplyVO vo = new ReplyVO();
+      
+      vo.setBno(bnoArr[i % 5]);
+      vo.setReply("´ñƒJ Å×½ºÆ® " + i);
+      vo.setReplyer("replyer " + i);
+      
+      mapper.insert(vo);
+    });
   }
 }
