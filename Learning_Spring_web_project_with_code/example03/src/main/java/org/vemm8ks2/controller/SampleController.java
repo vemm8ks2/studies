095983872ce64953be12eaf604515e89010cd38b
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 12:11:53 +0900

    FEAT: 16. REST 방식으로 전환 (16.2.2)
    16.2 @RestController의 반환 타입
    16.2.2 객체의 반환

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vemm8ks2.domain.SampleVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -19,4 +20,24 @@ public class SampleController {
 		
 		return "안녕하세요.";
 	}
+
+	@GetMapping(
+		value = "/getSample", 
+		produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+		}
+	)
+	public SampleVO getSample() {
+		
+		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
+		
+		return new SampleVO(112, "스타", "로드");
+	}
+	
+	@GetMapping(value = "/getSample2")
+	public SampleVO getSample2() {
+		return new SampleVO(113, "로켓", "라쿤");
+	}
+	
 }
