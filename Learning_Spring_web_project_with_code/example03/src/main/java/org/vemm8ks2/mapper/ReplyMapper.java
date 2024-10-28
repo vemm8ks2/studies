5cd5821cf0e54497aefe5204d330c8de5efcb2f0
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 18:07:37 +0900

    FEAT, TEST: 17. Ajax 댓글 처리 (17.2.4)
    17.2 댓글 처리를 위한 영속 영역
    17.2.4 @Param어노테이션과 댓글 목록

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/mapper/ReplyMapper.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/mapper/ReplyMapper.java
@@ -1,14 +1,19 @@
 package org.vemm8ks2.mapper;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.vemm8ks2.domain.Criteria;
 import org.vemm8ks2.domain.ReplyVO;
 
 public interface ReplyMapper {
 
   public int insert(ReplyVO vo);
-  
+
   public ReplyVO read(Long rno); // 특정 댓글 읽기
-  
+
   public int delete(Long rno);
-  
+
   public int update(ReplyVO reply);
+
+  public List<ReplyVO> getListWithPaging(@Param("cri") Criteria cri, @Param("bno") Long bno);
 }
--- Learning_Spring_web_project_with_code/example03/src/test/java/org/vemm8ks2/mapper/ReplyMapperTests.java
+++ Learning_Spring_web_project_with_code/example03/src/test/java/org/vemm8ks2/mapper/ReplyMapperTests.java
@@ -1,11 +1,13 @@
 package org.vemm8ks2.mapper;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.vemm8ks2.domain.Criteria;
 import org.vemm8ks2.domain.ReplyVO;
 import lombok.extern.log4j.Log4j;
 
@@ -16,59 +18,69 @@ public class ReplyMapperTests {
 
   @Autowired
   private ReplyMapper mapper;
-  
-  private Long[] bnoArr = { 524312L, 524311L, 524310L, 524309L, 524308L }; 
+
+  private Long[] bnoArr = {524312L, 524311L, 524310L, 524309L, 524308L};
 
   @Test
   public void testMapper() {
-    
+
     log.info(mapper);
   }
-  
+
   @Test
   public void testCreate() {
-    
+
     IntStream.rangeClosed(1, 10).forEach(i -> {
-      
+
       ReplyVO vo = new ReplyVO();
-      
+
       vo.setBno(bnoArr[i % 5]);
       vo.setReply("´ñƒJ Å×½ºÆ® " + i);
       vo.setReplyer("replyer " + i);
-      
+
       mapper.insert(vo);
     });
   }
-  
+
   @Test
   public void testRead() {
-    
+
     Long targetRno = 5L;
-    
+
     ReplyVO vo = mapper.read(targetRno);
-    
+
     log.info(vo);
   }
-  
+
   @Test
   public void testDelete() {
-    
+
     Long targetRno = 1L;
-    
+
     mapper.delete(targetRno);
+  }
+
+  @Test
+  public void testUpdate() {
+
+    Long targetRno = 10L;
+
+    ReplyVO vo = mapper.read(targetRno);
+
+    vo.setReply("Update Reply");
+
+    int count = mapper.update(vo);
+
+    log.info("UPDATE COUNT: " + count);
   }
   
   @Test
-  public void testUpdate() {
+  public void testList() {
     
-    Long targetRno = 10L;
+    Criteria cri = new Criteria();
     
-    ReplyVO vo = mapper.read(targetRno);
+    List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
     
-    vo.setReply("Update Reply");
-    
-    int count = mapper.update(vo);
-    
-    log.info("UPDATE COUNT: " + count);
+    replies.forEach(reply -> log.info(reply));
   }
 }
