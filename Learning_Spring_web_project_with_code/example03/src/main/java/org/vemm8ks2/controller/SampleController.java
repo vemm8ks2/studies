23967adfb03f36fd1ce81f380021c9271de19194
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 11:43:50 +0900

    FEAT: 16. REST 방식으로 전환 (16.2)
    16.2 @RestController의 반환 타입

---
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
@@ -0,0 +1,13 @@
+package org.vemm8ks2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+
+}
