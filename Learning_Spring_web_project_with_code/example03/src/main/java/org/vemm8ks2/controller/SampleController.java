2f92295d2d01f7d0261c38ad56788921dfef0cce
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 12:03:07 +0900

    FEAT: 16. REST 방식으로 전환 (16.2.1)
    16.2 @RestController의 반환 타입
    16.2.1 단순 문자열 반환
    
    Tomcat --> Modules --> Path를 '/'로 변경

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.vemm8ks2.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,11 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText() {
+		
+		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
+		
+		return "æ»≥Á«œººø‰.";
+	}
 }
