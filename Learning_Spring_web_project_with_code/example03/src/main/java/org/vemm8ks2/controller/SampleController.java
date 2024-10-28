8db68ec92ff4735196f7fe1e8150ad52d7db9855
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-28 12:18:44 +0900

    FEAT: 16. REST 방식으로 전환 (16.2.3)
    16.2 @RestController의 반환 타입
    16.2.3 컬렉션 타입의 객체 반환

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/SampleController.java
@@ -1,5 +1,11 @@
 package org.vemm8ks2.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +44,23 @@ public class SampleController {
 	@GetMapping(value = "/getSample2")
 	public SampleVO getSample2() {
 		return new SampleVO(113, "로켓", "라쿤");
+	}
+	
+	@GetMapping(value = "/getList")
+	public List<SampleVO> getList() {
+		return IntStream
+				.range(1, 10)
+				.mapToObj(i -> new SampleVO(i, i + " First", i + " Last"))
+				.collect(Collectors.toList());
+	}
+	
+	@GetMapping(value = "/getMap")
+	public Map<String, SampleVO> getMap() {
+		
+		Map<String, SampleVO> map = new HashMap<>();
+		map.put("First", new SampleVO(111, "그루트", "주니어"));
+		
+		return map;
 	}
 	
 }
