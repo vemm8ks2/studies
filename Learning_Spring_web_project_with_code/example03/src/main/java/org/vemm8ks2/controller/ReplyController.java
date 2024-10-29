5212f65858a42a0f929d0a7f449aace44a863320
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-29 10:08:46 +0900

    FEAT: 17. Ajax 댓글 처리 (17.3.5)
    17.3 서비스 영역과 Controller 처리
    17.3.5 댓글 수정

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.vemm8ks2.domain.Criteria;
 import org.vemm8ks2.domain.ReplyVO;
@@ -68,6 +69,19 @@ public class ReplyController {
     log.info("remove: " + rno);
 
     return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+        : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH}, value = "/{rno}",
+      consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+  public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+
+    vo.setRno(rno);
+
+    log.info("rno: " + rno);
+    log.info("modify: " + vo);
+
+    return service.modify(vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
         : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
