54b796f56f0d0564b85a7069ab443e76b425a017
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-29 10:02:28 +0900

    FEAT: 17. Ajax 댓글 처리 (17.3.4)
    17.3 서비스 영역과 Controller 처리
    17.3.4 댓글 삭제/조회

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,23 @@ public class ReplyController {
     log.info(cri);
 
     return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/{rno}",
+      produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+  public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+
+    log.info("get: " + rno);
+
+    return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+  }
+
+  @DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+  public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+
+    log.info("remove: " + rno);
+
+    return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+        : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
