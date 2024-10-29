27b54cc61daa6bdb2c9a4542cce39649ec73af05
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-29 09:48:49 +0900

    FEAT: 17. Ajax 댓글 처리 (17.3.2)
    17.3 서비스 영역과 Controller 처리
    17.3.2 등록 작업과 테스트

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/ReplyController.java
@@ -1,7 +1,14 @@
 package org.vemm8ks2.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vemm8ks2.domain.ReplyVO;
+import org.vemm8ks2.service.ReplyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
@@ -11,5 +18,19 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class ReplyController {
 
-  private ReplyController service;
+  private ReplyService service;
+
+  @PostMapping(value = "/new", consumes = "application/json",
+      produces = {MediaType.TEXT_PLAIN_VALUE})
+  public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
+
+    log.info("ReplyVO: " + vo);
+
+    int insertCount = service.register(vo);
+
+    log.info("Reply INSERT COUNT: " + insertCount);
+
+    return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+        : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
