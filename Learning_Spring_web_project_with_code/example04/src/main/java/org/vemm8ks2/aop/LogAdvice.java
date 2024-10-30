10af9f3bd40c7ec336f94ba07eb654b98f6541af
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-30 15:28:57 +0900

    FEAT: 18. AOP라는 패러다임 (18.5)
    18.5 @Around와 ProceedingJoinPoint

--- Learning_Spring_web_project_with_code/example04/src/main/java/org/vemm8ks2/aop/LogAdvice.java
+++ Learning_Spring_web_project_with_code/example04/src/main/java/org/vemm8ks2/aop/LogAdvice.java
@@ -1,6 +1,9 @@
 package org.vemm8ks2.aop;
 
+import java.util.Arrays;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -29,5 +32,29 @@ public class LogAdvice {
 
     log.info("|| --- Exception ... !");
     log.info("|| --- exception: " + exception);
+  }
+  
+  @Around("execution(* org.vemm8ks2.service.SampleService*.*(..))")
+  public Object logTime(ProceedingJoinPoint pjp) {
+    
+    long start = System.currentTimeMillis();
+    
+    log.info("| --- Target: " + pjp.getTarget());
+    log.info("| --- Param: " + Arrays.toString(pjp.getArgs()));
+    
+    // invoke method
+    Object result = null;
+    
+    try {
+      result = pjp.proceed();
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+    
+    long end = System.currentTimeMillis();
+    
+    log.info("| --- Time: " + (end - start));
+    
+    return result;
   }
 }
