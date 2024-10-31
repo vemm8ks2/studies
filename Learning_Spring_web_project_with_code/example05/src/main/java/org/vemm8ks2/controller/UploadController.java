5195638a2acf7a9340b8c085807bf395d799d7af
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-31 12:24:12 +0900

    FEAT: 22. 파일 업로드 상세 처리 (22.1.2)
    22.1 파일의 확장자나 크기의 사전 처리
    22.1.2 년/월/일 폴더의 생성

--- Learning_Spring_web_project_with_code/example05/src/main/java/org/vemm8ks2/controller/UploadController.java
+++ Learning_Spring_web_project_with_code/example05/src/main/java/org/vemm8ks2/controller/UploadController.java
@@ -1,6 +1,8 @@
 package org.vemm8ks2.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,9 +47,15 @@ public class UploadController {
   @PostMapping("/uploadAjaxAction")
   public void uploadAjaxPost(MultipartFile[] uploadFile) {
     
-    log.info("|| --- update ajax post ...");
-    
     String uploadFolder = "C:\\upload";
+    
+    // make folder
+    File uploadPath = new File(uploadFolder, getFolder());
+    log.info("|| --- upload path: " + uploadPath);
+    
+    if (!uploadPath.exists()) {
+      uploadPath.mkdirs(); // make yyyy/MM/dd folder
+    }
     
     for (MultipartFile multipartFile : uploadFile) {
       
@@ -61,7 +69,7 @@ public class UploadController {
       uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
       log.info("|| --- Only file name: " + uploadFileName);
       
-      File saveFile = new File(uploadFolder, uploadFileName);
+      File saveFile = new File(uploadPath, uploadFileName);
       
       try {
         multipartFile.transferTo(saveFile);
@@ -69,5 +77,15 @@ public class UploadController {
         log.error(e.getMessage());
       }
     }
+  }
+  
+  private String getFolder() {
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    
+    Date date = new Date();
+    String str = sdf.format(date);
+    
+    return str.replace("-", File.separator);
   }
 }
