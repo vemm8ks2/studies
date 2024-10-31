fb476214f8424bb65562cde8c79a90cf1a85c31b
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-31 12:42:21 +0900

    FEAT: 22. 파일 업로드 상세 처리 (22.2.1)
    22.2 섬네일 이미지 생성
    22.2.1 이미지 파일의 판단

--- Learning_Spring_web_project_with_code/example05/src/main/java/org/vemm8ks2/controller/UploadController.java
+++ Learning_Spring_web_project_with_code/example05/src/main/java/org/vemm8ks2/controller/UploadController.java
@@ -1,6 +1,9 @@
 package org.vemm8ks2.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -44,51 +48,75 @@ public class UploadController {
   public void uploadAjax() {
     log.info("|| --- upload ajax");
   }
-  
+
   @PostMapping("/uploadAjaxAction")
   public void uploadAjaxPost(MultipartFile[] uploadFile) {
-    
+
     String uploadFolder = "C:\\upload";
-    
+
     // make folder
     File uploadPath = new File(uploadFolder, getFolder());
     log.info("|| --- upload path: " + uploadPath);
-    
+
     if (!uploadPath.exists()) {
       uploadPath.mkdirs(); // make yyyy/MM/dd folder
     }
-    
+
     for (MultipartFile multipartFile : uploadFile) {
-      
+
       log.info("||");
       log.info("|| --- Upload File Name: " + multipartFile.getOriginalFilename());
       log.info("|| --- Upload File Size: " + multipartFile.getSize());
-      
+
       String uploadFileName = multipartFile.getOriginalFilename();
-      
+
       // IE has file path
       uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
       log.info("|| --- Only file name: " + uploadFileName);
 
       UUID uuid = UUID.randomUUID();
       uploadFileName = uuid.toString() + "_" + uploadFileName;
-      File saveFile = new File(uploadPath, uploadFileName);
-      
+
       try {
+        File saveFile = new File(uploadPath, uploadFileName);
         multipartFile.transferTo(saveFile);
+
+        // check image type file
+        if (checkImageType(saveFile)) {
+          FileOutputStream thumnail =
+              new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+          
+          Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumnail, 100, 100);
+          
+          thumnail.close();
+        }
       } catch (Exception e) {
         log.error(e.getMessage());
       }
     }
   }
-  
+
   private String getFolder() {
-    
+
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    
+
     Date date = new Date();
     String str = sdf.format(date);
-    
+
     return str.replace("-", File.separator);
+  }
+
+  private boolean checkImageType(File file) {
+
+    try {
+      String contentType = Files.probeContentType(file.toPath());
+
+      return contentType.startsWith("image");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return false;
   }
 }
