9df537da254e3d0aa2a41dcefe29f751ce59950c
vemm8ks2 <vemm8ks2@gmail.com>
2024-10-31 09:50:25 +0900

    FEAT: 19. 스프링에서 트랜잭션 관리 (19.2.4)
    19.2 트랜잭션 설정 실습
    19.2.4 @Transactional 어노테이션

--- Learning_Spring_web_project_with_code/example04/src/main/java/org/vemm8ks2/service/SampleTxServiceImpl.java
+++ Learning_Spring_web_project_with_code/example04/src/main/java/org/vemm8ks2/service/SampleTxServiceImpl.java
@@ -2,6 +2,7 @@ package org.vemm8ks2.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.vemm8ks2.mapper.Sample1Mapper;
 import org.vemm8ks2.mapper.Sample2Mapper;
 import lombok.extern.log4j.Log4j;
@@ -16,6 +17,7 @@ public class SampleTxServiceImpl implements SampleTxService {
   @Autowired
   private Sample2Mapper mapper2;
 
+  @Transactional
   @Override
   public void addData(String value) {
 
