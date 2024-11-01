789733bdd8731c76a29fcb5c6965c660d8f3878e
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-01 12:35:25 +0900

    FEAT, STYLE: 26. 게시물의 조회와 첨부파일 (26.2)
    26.2 BoardController의 변경과 화면 처리

--- Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/BoardController.java
+++ Learning_Spring_web_project_with_code/example03/src/main/java/org/vemm8ks2/controller/BoardController.java
@@ -1,5 +1,9 @@
 package org.vemm8ks2.controller;
 
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.vemm8ks2.domain.BoardAttachVO;
 import org.vemm8ks2.domain.BoardVO;
 import org.vemm8ks2.domain.Criteria;
 import org.vemm8ks2.domain.PageDTO;
@@ -22,80 +27,84 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class BoardController {
 
-	private BoardService service;
-	
-	@GetMapping("/list")
-	public void list(Criteria cri, Model model) {
-		
-		log.info("list: " + cri);
-		
-		int total = service.getTotal(cri);
-		
-		log.info("total: " + total);
-		
-		model.addAttribute("list", service.getList(cri));
-		model.addAttribute("pageMaker", new PageDTO(cri, total));
-	}
-	
-	@GetMapping("/register")
-	public void register() {
-		
-	}
-	
-	@PostMapping("/register")
-	public String register(BoardVO board, RedirectAttributes attr) {
-		
-	    log.info("||");
-		log.info("|| --- register: " + board);
-		
-		if (board.getAttachList() != null) {
-		  board.getAttachList().forEach(attach -> log.info("|| --- " + attach));
-		}
-		
-		log.info("||");
-		
-		service.register(board);
-		
-		attr.addFlashAttribute("result", board.getBno());
-		
-		return "redirect:/board/list";
-	}
-	
-	@GetMapping({"/get", "/modify"})
-	public void get(@RequestParam("bno") Long bno,
-					@ModelAttribute("cri") Criteria cri,
-					Model model) {
+  private BoardService service;
 
-		log.info("/get or /modify");
-		
-		model.addAttribute("board", service.get(bno));
-	}
-	
-	@PostMapping("/modify")
-	public String modify(BoardVO board, 
-						 @ModelAttribute("cri") Criteria cri, 
-						 RedirectAttributes attr) {
-		
-		log.info("modify: " + board);
-		
-		if (service.modify(board)) {
-			attr.addFlashAttribute("result", "success");
-		}
-		
-		return "redirect:/board/list" + cri.getListLink();
-	}
-	
-	@PostMapping("/remove")
-	public String remove(@RequestParam("bno") Long bno, 
-						 @ModelAttribute("cri") Criteria cri, 
-						 RedirectAttributes attr) {
-		
-		log.info("remove ... " + bno);
-		
-		if (service.remove(bno)) {
-			attr.addFlashAttribute("result", "success");
-		}
-		
-		return "redirect:/board/list" + cri.getListLink();
-	}
+  @GetMapping("/list")
+  public void list(Criteria cri, Model model) {
+
+    log.info("list: " + cri);
+
+    int total = service.getTotal(cri);
+
+    log.info("total: " + total);
+
+    model.addAttribute("list", service.getList(cri));
+    model.addAttribute("pageMaker", new PageDTO(cri, total));
+  }
+
+  @GetMapping("/register")
+  public void register() {
+
+  }
+
+  @PostMapping("/register")
+  public String register(BoardVO board, RedirectAttributes attr) {
+
+    log.info("||");
+    log.info("|| --- register: " + board);
+
+    if (board.getAttachList() != null) {
+      board.getAttachList().forEach(attach -> log.info("|| --- " + attach));
+    }
+
+    log.info("||");
+
+    service.register(board);
+
+    attr.addFlashAttribute("result", board.getBno());
+
+    return "redirect:/board/list";
+  }
+
+  @GetMapping({"/get", "/modify"})
+  public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+
+    log.info("/get or /modify");
+
+    model.addAttribute("board", service.get(bno));
+  }
+
+  @PostMapping("/modify")
+  public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri,
+      RedirectAttributes attr) {
+
+    log.info("modify: " + board);
+
+    if (service.modify(board)) {
+      attr.addFlashAttribute("result", "success");
+    }
+
+    return "redirect:/board/list" + cri.getListLink();
+  }
+
+  @PostMapping("/remove")
+  public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri,
+      RedirectAttributes attr) {
+
+    log.info("remove ... " + bno);
+
+    if (service.remove(bno)) {
+      attr.addFlashAttribute("result", "success");
+    }
+
+    return "redirect:/board/list" + cri.getListLink();
+  }
+
+  @GetMapping(value = "/getAttachList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<List<BoardAttachVO>> getAttachList(Long bno) {
+
+    log.info("|| --- getAttachList " + bno);
+
+    return new ResponseEntity<>(service.getAttachList(bno), HttpStatus.OK);
+  }
 }
