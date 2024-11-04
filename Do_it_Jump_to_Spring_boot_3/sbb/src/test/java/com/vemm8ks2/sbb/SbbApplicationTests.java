569768150b7c637638d23e185a2e303d5836ce98
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-04 10:41:24 +0900

    FEAT: 01. 스프링 부트 개발 준비하기 (1-02)
    1-02. 스프링 부트 개발 환경 준비하기
    
    JDK 설치하기
    STS 설치하기
    STS 실행하기
    스프링 부트 프로젝트 만들기

---
+++ Do_it_Jump_to_Spring_boot_3/sbb/src/test/java/com/vemm8ks2/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemm8ks2.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
