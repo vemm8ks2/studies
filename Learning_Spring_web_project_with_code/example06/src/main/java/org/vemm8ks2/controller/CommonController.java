b6b590c87148ddcc5479db34d64e8243d08f3e32
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-05 17:48:48 +0900

    FEAT: 31. 로그인과 로그아웃 처리 (31.3)
    31.3 커스텀 로그인 페이지

--- Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/CommonController.java
+++ Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/CommonController.java
@@ -12,8 +12,24 @@ public class CommonController {
 
   @GetMapping("/accessError")
   public void accessDenied(Authentication auth, Model model) {
-    log.info("|| --- Access Denieid: " + auth);
     
+    log.info("|| --- Access Denieid: " + auth);
+
     model.addAttribute("msg", "Access Denied");
+  }
+
+  @GetMapping("customLogin")
+  public void loginInput(String error, String logout, Model model) {
+    
+    log.info("|| --- error: " + error);
+    log.info("|| --- logout: " + logout);
+    
+    if (error != null) {
+      model.addAttribute("error", "Login Error Check Your Account");
+    }
+    
+    if (logout != null) {
+      model.addAttribute("logout", "Logout!");
+    }
   }
 }
