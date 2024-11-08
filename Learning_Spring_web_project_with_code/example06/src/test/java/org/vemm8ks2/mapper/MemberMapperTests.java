da964776c61046ee2a037ef466b28256b47e2fd2
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-08 12:43:17 +0900

    TEST: 33. 커스텀 UserDetailsService 활용 (33.1.2)
    33.1 회원 도메인, 회원 Mapper 설계
    33.1.2 MemberMapper 테스트

---
+++ Learning_Spring_web_project_with_code/example06/src/test/java/org/vemm8ks2/mapper/MemberMapperTests.java
@@ -0,0 +1,28 @@
+package org.vemm8ks2.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.vemm8ks2.domain.MemberVO;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+@Log4j
+public class MemberMapperTests {
+
+  @Autowired
+  private MemberMapper mapper;
+
+  @Test
+  public void testRead() {
+    
+    MemberVO vo = mapper.read("admin90");
+    
+    log.info("|| --- " + vo);
+    
+    vo.getAuthList().forEach(authVO -> log.info("|| --- " + authVO));
+  }
+}
