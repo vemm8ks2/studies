b2df26b043e3d2188114749425c5b9d1d42acfc6
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-08 13:55:06 +0900

    FEAT: 33. 커스텀 UserDetailsService 활용 (33.2.1)
    33.2 CustomUserDetailsService 구성
    33.2.1 MemberVO를 UsersDetails 타입으로 변환하기

--- Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/security/CustomUserDetailsService.java
+++ Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/security/CustomUserDetailsService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.vemm8ks2.domain.MemberVO;
 import org.vemm8ks2.mapper.MemberMapper;
+import org.vemm8ks2.security.domain.CustomUser;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
@@ -12,13 +14,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Autowired
   private MemberMapper memberMapper;
-  
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    
+
     log.warn("|| --- Load User By Username : " + username);
-    
-    return null;
+
+    // username means userid
+    MemberVO vo = memberMapper.read(username);
+
+    log.warn("|| --- Queried by member mapper : " + vo);
+
+    return vo == null ? null : new CustomUser(vo);
   }
 
 }
