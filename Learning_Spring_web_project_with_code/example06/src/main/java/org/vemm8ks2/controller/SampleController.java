a9be47e7e057746789aed5ed0c4743765933cf95
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-11 12:54:48 +0900

    FEAT: 37. 어노테이션을 이용하는 스프링 시큐리티 설정 (37)

--- Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/SampleController.java
+++ Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.vemm8ks2.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +14,31 @@ public class SampleController {
 
   @GetMapping("/all")
   public String doAll() {
-    log.info("do all can access everybody");
+    log.info("|| --- do all can access everybody");
     return "all";
   }
-  
+
   @GetMapping("/member")
   public String doMember() {
-    log.info("logged in member");
+    log.info("|| --- logged in member");
     return "member";
   }
-  
+
   @GetMapping("/admin")
   public String doAdmin() {
-    log.info("admin only");
+    log.info("|| --- admin only");
     return "admin";
+  }
+
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+  @GetMapping("/annoMember")
+  public void doMember2() {
+    log.info("|| --- logined annotation member");
+  }
+
+  @Secured({"ROLE_ADMIN"})
+  @GetMapping("/annoAdmin")
+  public void doAdmin2() {
+    log.info("|| --- admin annotation only");
   }
 }
