61803920700e811820c5674f4fcf24ac6e29175a
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-11 10:40:57 +0900

    FEAT: 03. SBB 서비스 개발하기 (3-05)
    3-05. 스프링 시큐리티란?
    
    스프링 시큐리티 설치하기
    스프링 시큐리티 설정하기
    H2 콘솔 오류 수정하기

---
+++ Do_it_Jump_to_Spring_boot_3/sbb/src/main/java/com/vemm8ks2/sbb/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.vemm8ks2.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // 스프링의 환경 설정 파일임을 의미
+@EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받음. 스프링 시큐리티를 활성화하는 역할을 함.
+public class SecurityConfig {
+
+  /*
+   * 내부적으로 SecurityFilterChain 클래스가 동작하여 모든 요청 URL에 이 클래스가 필터로 적용되어 URL 별로 틀벽할 설정이 가능해진다. 세부
+   * 설정은 @Bean을 통해 SecurityFilterChain 빈을 생성하여 설정 가능하다.
+   */
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        .csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+        .headers((headers) -> headers.addHeaderWriter(
+            new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+
+    return http.build();
+  }
+}
