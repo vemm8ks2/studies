d395f8e8fac9be1b12b81ef3eb98487f354b44aa
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-12 13:52:17 +0900

    FEAT: 38. 기존 프로젝트에 스프링 시큐리티 접목하기 (38.4.2)
    38.4 게시물의 수정/삭제
    38.4.2 BoardController에서의 제어

--- Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/BoardController.java
+++ Learning_Spring_web_project_with_code/example06/src/main/java/org/vemm8ks2/controller/BoardController.java
@@ -80,9 +80,9 @@ public class BoardController {
     model.addAttribute("board", service.get(bno));
   }
 
+  @PreAuthorize("principal.username == #board.writer")
   @PostMapping("/modify")
-  public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri,
-      RedirectAttributes attr) {
+  public String modify(BoardVO board, Criteria cri, RedirectAttributes attr) {
 
     log.info("|| --- modify: " + board);
 
@@ -93,12 +93,13 @@ public class BoardController {
     return "redirect:/board/list" + cri.getListLink();
   }
 
+  @PreAuthorize("principal.username == #writer")
   @PostMapping("/remove")
   public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri,
       RedirectAttributes attr) {
 
     log.info("|| --- remove ... " + bno);
-    
+
     List<BoardAttachVO> attachList = service.getAttachList(bno);
 
     if (service.remove(bno)) {
