ad373c8c463b10e309df29bab8ab3986e4ad7b46
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-14 10:00:12 +0900

    FEAT: 03. SBB 서비스 개발하기 (3-12)
    3-12. 마크다운 적용하기
    
    마크다운 설치하기
    마크다운 컴포넌트 작성하기
    템플릿에 마크다운 적용하기
    마크다운 확인하기

---
+++ Do_it_Jump_to_Spring_boot_3/sbb/src/main/java/com/vemm8ks2/sbb/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.vemm8ks2.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+  public String markdown(String markdown) {
+    
+    Parser parser = Parser.builder().build();
+    Node document = parser.parse(markdown);
+    HtmlRenderer renderer = HtmlRenderer.builder().build();
+    
+    return renderer.render(document);
+  }
+}
