c04ddc55a42f8cec6a1ee61146e0eee98cdc7f94
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-14 11:44:18 +0900

    FEAT: 03. 스프링과 Oracle Database 연동 (3.4.1)
    3.4 커넥션 풀 설정
    3.4.1 라이브러리 추가와 DataSource 설정
    
    Java 설정을 이용하는 경우

--- Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
+++ Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
@@ -1,10 +1,28 @@
 package org.vemm8ks2.config;
 
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.vemm8ks2.sample"})
 public class RootConfig {
 
+  @Bean
+  public DataSource dataSource() {
+    
+    HikariConfig hikariConfig = new HikariConfig();
+    
+    hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+    hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+    hikariConfig.setUsername("web");
+    hikariConfig.setPassword("1234");
+    
+    HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+    
+    return dataSource;
+  }
 }
