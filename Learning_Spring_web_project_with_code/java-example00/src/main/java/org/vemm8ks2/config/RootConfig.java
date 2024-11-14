113015fdb0b194cbf8808f4b61ba7a5f5a03efd7
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-14 11:29:20 +0900

    FEAT: 02. 스프링의 특징과 의존성 주입 (2.2.2)
    2.2 의존성 주입 테스트
    2.2.2 XML을 이용하는 의존성 주입 설정
    
    Java 설정을 이용하는 의존성 주입

--- Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
+++ Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
@@ -1,8 +1,10 @@
 package org.vemm8ks2.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"org.vemm8ks2.sample"})
 public class RootConfig {
 
 }
