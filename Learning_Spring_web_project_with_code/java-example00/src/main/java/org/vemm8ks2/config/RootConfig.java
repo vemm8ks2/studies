aec9838279f19e8642098aeb83fd0bb649318647
vemm8ks2 <vemm8ks2@gmail.com>
2024-11-14 11:50:08 +0900

    FEAT: 04. MyBatis와 스프링 연동 (4.1.1, 4.1.2)
    4.1 MyBatis
    4.1.1 MyBatis 관련 라이브러리 추가
    4.1.2 SQLSessionFactory
    
    Java 설정을 이용하는 경우

--- Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
+++ Learning_Spring_web_project_with_code/java-example00/src/main/java/org/vemm8ks2/config/RootConfig.java
@@ -1,6 +1,8 @@
 package org.vemm8ks2.config;
 
 import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,16 +15,25 @@ public class RootConfig {
 
   @Bean
   public DataSource dataSource() {
-    
+
     HikariConfig hikariConfig = new HikariConfig();
-    
+
     hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
     hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
     hikariConfig.setUsername("web");
     hikariConfig.setPassword("1234");
-    
+
     HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-    
+
     return dataSource;
+  }
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+
+    SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+    sqlSessionFactory.setDataSource(dataSource());
+
+    return (SqlSessionFactory) sqlSessionFactory.getObject();
   }
 }
